1ae6f38832e425fd2ff2cfaf7a9df44078c70d37
Abdelrahman Wael <55349570+AWsabry@users.noreply.github.com>
2021-05-01 04:31:52 +0200

    Update index.py

--- index.py
+++ index.py
@@ -32,14 +32,6 @@ db = firestore.client()
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route("/index")
-def index():
-    from google.cloud import firestore
-    docs = db.collection(u'users').order_by(
-        u'createdAt', direction=firestore.Query.DESCENDING).stream()
-    return render_template('index.html', docs=docs)
-
-
 @app.route("/login", methods=['POST', 'GET'])
 def login():
     if request.method == "POST":
